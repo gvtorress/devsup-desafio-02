3e4908985bafeac67618b01d7810d8ae6a364fff
Gabriel Torres <gvtorress@gmail.com>
2025-01-04 22:56:41 -0300

    create recursive function totalSales

---
+++ desafio02.js
@@ -0,0 +1,34 @@
+function totalSales(sales, department) {
+  const count = 0;
+  const total = 0;
+  return recursiveTotalSales(sales, department, count, total);
+}
+
+function recursiveTotalSales(sales, department, count, total) {
+  if (sales.length === 0) return [count, total];
+  const sale = sales[0];
+  if (sale[3] === department) {
+    count += 1;
+    total += Number(sale[2]);
+  }
+  newSales = sales.slice(1);
+  return recursiveTotalSales(newSales, department, count, total);
+}
+
+const data = {
+  "sales": [
+    "8349,14/09/2024,899.9,ESPORTE",
+    "4837,17/09/2024,530.0,VESTUARIO",
+    "15281,21/09/2024,1253.99,ESPORTE",
+    "15344,27/09/2024,1000.9,VESTUARIO",
+    "18317,04/10/2024,250.4,VESTUARIO",
+    "18972,11/10/2024,385.5,JARDINAGEM"
+  ],
+  "department": "VESTUARIO"
+};
+
+const sales = data.sales.map((s) => s.split(","));
+const department = data.department;
+
+const result = totalSales(sales, department);
+console.log(result);
